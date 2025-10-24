d65f2c8434f887b55e3f1e5e044031632c69a5e3
DanielWewen <danielsepthiady66@gmail.com>
2025-10-25 00:30:13 +0700

    konfigurasi mini project

---
+++ js/script.js
@@ -0,0 +1,22 @@
+welcomeSpeech();
+
+function welcomeSpeech() {
+    let name= prompt("enter your name:");
+
+    document.getElementById('greet-name').innerHTML = `Hi ${name},`;
+
+}
+
+function validateForm () {
+    const name = document.getElementById('name-input').value;
+    const email = document.getElementById('email-input').value;
+    const message = document.getElementById('message-input').value;
+
+    if (name === "" || email === "" || message === "") {
+        alert ("All fields are required!");
+    } else {
+        alert(`Thank you ${name}, form submitted successfully`);
+
+       
+    }
+}
